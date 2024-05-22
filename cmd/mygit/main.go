cfbf6ee1f9700144f927c8cc586d3286c20ea8a3
Paras Raba <parasgadhvi12345@gmail.com>
2024-05-22 11:01:22 +0530

    Add stage 2 `Read a blob object`

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"compress/zlib"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 )
 
 // Usage: your_git.sh <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	// Uncomment this block to pass the first stage!
 	//
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
+		ePrintf("usage: mygit <command> [<args>...]\n")
 		os.Exit(1)
 	}
 
@@ -21,19 +21,70 @@ func main() {
 	case "init":
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 			if err := os.MkdirAll(dir, 0755); err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+				ePrintf("Error creating directory: %s\n", err)
 			}
 		}
 
 		headFileContents := []byte("ref: refs/heads/main\n")
 		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+			ePrintf("Error writing file: %s\n", err)
 		}
 
 		fmt.Println("Initialized git directory")
 
+	case "cat-file":
+		if len(os.Args) != 4 {
+			ePrintf("usage: mygit cat-file <flag> <file>\n")
+			os.Exit(1)
+		}
+		if os.Args[2] != "-p" {
+			ePrintf("usage: mygit cat-file -p <file>\n")
+			os.Exit(1)
+		}
+		objHash := os.Args[3]
+		if len(objHash) != 40 {
+			ePrintf("invalid object hash: %q", objHash)
+			os.Exit(1)
+		}
+		file, err := os.Open(fmt.Sprintf(".git/objects/%s/%s", objHash[0:2], objHash[2:]))
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				ePrintf("no such object: %q", objHash)
+				os.Exit(1)
+			}
+			ePrintf("could not open the object file: %v", err)
+			os.Exit(1)
+		}
+		readObjectFile(file)
+		file.Close()
+
 	default:
-		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
+		ePrintf("Unknown command %s\n", command)
 		os.Exit(1)
 	}
+}
+
+func ePrintf(format string, a ...any) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+func readObjectFile(r io.Reader) error {
+	z, err := zlib.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer z.Close()
+	content, err := io.ReadAll(z)
+	if err != nil {
+		return err
+	}
+	zeroPos := 0
+	for _, by := range content {
+		if by == 0 {
+			break
+		}
+		zeroPos++
+	}
+	ePrintf("%s", content[zeroPos+1:])
+	return nil
 }
