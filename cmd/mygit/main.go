f6a2ec7ccc3715d6506f709e3f284b0f42535025
Paras Raba <parasgadhvi12345@gmail.com>
2025-01-20 08:17:25 +0530

    ADD stage 3 Create blob object

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 )
 
@@ -47,6 +51,7 @@ func main() {
 			os.Exit(1)
 		}
 		file, err := os.Open(fmt.Sprintf(".git/objects/%s/%s", objHash[0:2], objHash[2:]))
+		defer file.Close()
 		if err != nil {
 			if errors.Is(err, os.ErrNotExist) {
 				ePrintf("no such object: %q", objHash)
@@ -55,8 +60,49 @@ func main() {
 			ePrintf("could not open the object file: %v", err)
 			os.Exit(1)
 		}
-		readObjectFile(file)
-		file.Close()
+		err = readObjectFile(file)
+		if err != nil {
+			ePrintf("error in reading the object file: %s", err)
+			os.Exit(1)
+		}
+
+	case "hash-object":
+		if len(os.Args) != 4 {
+			ePrintf("usage: mygit cat-file <flag> <file>\n")
+			os.Exit(1)
+		}
+		if os.Args[2] != "-w" {
+			ePrintf("usage: mygit cat-file -p <file>\n")
+			os.Exit(1)
+		}
+		file, err := os.Open(os.Args[3])
+		if err != nil {
+			ePrintf("error in opening the given file: %s", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		fileContent, err := io.ReadAll(file)
+		if err != nil {
+			ePrintf("error in reading the given file: %s", err)
+			os.Exit(1)
+		}
+		contentToWrite := createContentWithInfo("blob", fileContent)
+		fileSHA, err := calculateSHA(contentToWrite)
+		if err != nil {
+			ePrintf("error in calculating the SHA: %s", err)
+			os.Exit(1)
+		}
+		nFile, err := createEmptyObjectFile(fileSHA)
+		if err != nil {
+			ePrintf("error in creating the object file: %s", err)
+			os.Exit(1)
+		}
+		err = createObjectFile(nFile, bytes.NewReader(contentToWrite))
+		if err != nil {
+			ePrintf("error in writing to the object file: %s", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", fileSHA)
 
 	default:
 		ePrintf("Unknown command %s\n", command)
@@ -87,4 +133,75 @@ func readObjectFile(r io.Reader) error {
 	}
 	fmt.Printf("%s", content[zeroPos+1:])
 	return nil
+}
+
+func createObjectFile(w io.Writer, content io.Reader) error {
+	z := zlib.NewWriter(w)
+	defer z.Close()
+
+	contentByte, err := io.ReadAll(content)
+	if err != nil {
+		return fmt.Errorf("createObjectFile file could not read the content: %s", err)
+	}
+
+	n, err := z.Write(contentByte)
+	if err != nil {
+		return fmt.Errorf("createObjectFile file could not write the content: %s", err)
+	}
+	if n != len(contentByte) {
+		return fmt.Errorf("createObjectFile content length and written bytes do not match %d and %d", len(contentByte), n)
+	}
+	return nil
+}
+
+func calculateSHA(content []byte) (string, error) {
+	hasher := sha1.New()
+	n, err := hasher.Write(content)
+	if err != nil {
+		return "", err
+	}
+	if n != len(content) {
+		return "", fmt.Errorf("mismatch in the bytes written and content: %d and %d", n, len(content))
+	}
+	sha := hex.EncodeToString(hasher.Sum(nil))
+	return sha, nil
+}
+
+func createEmptyObjectFile(sha string) (*os.File, error) {
+	if len(sha) != 40 {
+		return nil, fmt.Errorf("invalid length of sha object: %d", len(sha))
+	}
+	dir, rest := sha[0:2], sha[2:]
+	err := os.Mkdir(fmt.Sprintf("./.git/objects/%s", dir), fs.FileMode(os.ModeDir))
+	if err != nil {
+		return nil, err
+	}
+	return os.Create(fmt.Sprintf("./.git/objects/%s/%s", dir, rest))
+}
+
+func createContentWithInfo(typ string, content []byte) []byte {
+	contentLength := len(content)
+	contentDigitLength := numOfDigits(contentLength)
+
+	result := make([]byte, 0, len(typ)+contentLength+2+len(content))
+	// append type
+	result = append(result, typ...)
+	// append the space
+	result = append(result, ' ')
+	// append the size
+	result = append(result, []byte(fmt.Sprintf("%d", contentLength))...)
+	// append the null byte
+	result = append(result, 0)
+	// append the content
+	result = append(result, content...)
+	return result
+}
+
+func numOfDigits(a int) int {
+	count := 0
+	for a != 0 {
+		a /= 10
+		count++
+	}
+	return count
 }
